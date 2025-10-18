13cf7933d78d086228954972ac611218622cc91d
Kishoreravi2007 <92873158+Kishoreravi2007@users.noreply.github.com>
2025-10-18 22:38:33 +0530

    27th a

--- frontend/src/pages/sister-a/Layout.tsx
+++ frontend/src/pages/sister-a/Layout.tsx
@@ -11,6 +11,9 @@ const SisterALayout = () => {
         <main className="pb-20 md:pb-0">
           <Outlet />
         </main>
+        <div className="fixed bottom-20 left-4 text-[#8C3B3B] text-sm bg-white p-2 rounded z-20">
+          <a href="mailto:help.weddingweb@gmail.com" className="underline">help.weddingweb@gmail.com</a>
+        </div>
         <BottomNav basePath="/parvathy" activeColor="#D4AF37" />
       </div>
     </div>
